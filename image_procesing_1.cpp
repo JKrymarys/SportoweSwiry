623dd32c5e5bf7666601b1d7afa7a26a539221b4
Argh4k <wyrzuc.maciej@gmail.com>
2016-10-04 11:21:48 +0200

    Zaczynamy powaznie

--- image_procesing_1.cpp
+++ image_procesing_1.cpp
@@ -5,14 +5,31 @@ using namespace std;
 
 int main( int argc, char * argv[] ){
 
-string arg1(argv[1]);
-if(arg1 == "-help")
-	cout << "Prosze";
-if((argc == 2))
+if (argc < 2 || argc > 3)
 {
-	cout<<"Pomoc, tepy chuju"<<endl;
+	cout << "The number of arguments is not correct ";
+	return 0;
 }
-cout << "dupa" << argv[1] << "dupa";
+string arg1(argv[1]);
+if (argc == 2 && arg1 == "--help")
+{
+	cout << "There will be help"
+		return 0;
+}
+if (argc == 3)
+{
+	string arg2(argv[2]);
+	//image.open(arg1)
+	//switch (arg2)
+	//{
+	//case "--brightness":
+	//	break;
+	//case "--constrast":
+	//	break;
+	//case "--neagtive":
+	//	break;
+	//}
+}
 system("pause");
 return 0;
 }
