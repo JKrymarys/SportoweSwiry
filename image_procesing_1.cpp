ae6e1e72ca05db6bef0758e66985d355d3151b50
Krymek <kuba.krymarys@gmail.com>
2016-10-04 10:30:47 +0200

    argc i argv

---
+++ image_procesing_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main( int argc, char * argv[] ){
+
+if(argc == 2 && argv[1]=="-help")
+{
+	cout<<"Pomoc, tepy chuju"<<endl;
+}
+	cout<< argc<<endl;
+system("pause");
+return 0;
+}
