2574e0e7f8180050236f562ad6c1a02424b0d7ac
Argh4k <wyrzuc.maciej@gmail.com>
2016-10-08 15:23:46 +0200

    Dodano funkcje uzywajace wskaznikow do funkcji

--- Image processing/newhope.cpp
+++ Image processing/newhope.cpp
@@ -1,5 +1,3 @@
-
-
 #include "stdafx.h"
 #include <iostream>
 #include <string>
@@ -8,21 +6,68 @@
 using namespace cimg_library;
 using namespace std;
 
-
+const int ArrSize = 255;
 
 //declarations
 
-bool Check_boundary(CImg<float> & image, int & x, int & y, int & c, float & level) {
 
-	if (image(x, y, 0, c) + level < 0) { image(x, y, 0, c) = 0;  return true; }
-	else if (image(x, y, 0, c) + level > 255) { image(x, y, 0, c) = 255; return true; }
-	else { return false; }
 
+// FUNCKJA TWORZACA LUT W ZALEZNOSCI OD PRZEKAZANEJ OPERACJI
+int * lookuptable(float level, float(*operation)(float, float))
+{
+	int * lut = new int[ArrSize];
+	{
+		for (int i = 0; i < ArrSize; i++)
+		{
+			lut[i] = operation(i, level);
+		}
+
+		cout << endl << "LOOKUPTABLE TEST" << endl;
+
+		for (int i = 0; i < ArrSize; i++)
+		{
+			cout << lut[i] << endl;
+		}
+
+	}
+	return lut;
 }
 
+// MOZLIWE OPERACJE ZWIAZANE Z LUTEM
 
-void brightness(float level, CImg <float> & image) {
+float brightlut(float value, float level)
+{
+	if (value + level < 0) { return 0; }
+	else if (value + level > 255) { return 255; }
+	else return value + level;
+}
 
+float contrastlut(float value, float level)
+{
+	if (value < 127)
+	{
+		return value - level < 0 ? 0 : value - level;
+	}
+	if (value >= 127)
+	{
+		return value + level > 255 ? 255 : value + level;
+	}
+}
+
+float negativelut(float value, float level = 0)
+{
+	if (value < 127) return value + 127;
+	else return value - 127;
+}
+
+// ARRAY OF POINTERS TO FUNCTIONS
+float(*operations[3])(float, float) = { brightlut, contrastlut, negativelut };
+
+
+// PRZECHODZI PRZEZ WSZYSTKIE PIXELE WCZESNIEJ TWORZAC LUT ODPOWIEDNIE DO OPERACJI
+void basicoperations(float level, CImg <float> & image, float(*operation)(float, float)) {
+
+	int * lut = lookuptable(level, operation);
 
 	for (int x = 0; x < image.width(); x++)
 	{
@@ -30,24 +75,20 @@ void brightness(float level, CImg <float> & image) {
 		{
 			for (int c = 0; c < 3; c++)
 			{
-				if (!Check_boundary(image, x, y, c, level))
-				{
-					image(x, y, 0, c) += level;
-				}
-
+				image(x, y, 0, c) = lut[ (int)image(x, y, 0, c) ];
 			}
-
-
 		}
 	}
-
 }
+
+
+
 
 
 
 int main(int argc, char * argv[]) {
 
-	cout << "Hello in our program \n <if you need help, plese use -help >" << endl;
+	cout << "Hello in our program \n <if you need help, plese use --help >" << endl;
 	string operation_to_do;
 	string path_to_file;
 	float modificator;
@@ -78,18 +119,27 @@ int main(int argc, char * argv[]) {
 
 	// CHOISE OF THE OPERATION
 
-	if ( (string)argv[2] == "--brightness")
+	if ((string)argv[2] == "--brightness")
 	{
 		cout << "Give level of brighting " << endl;
 		cin >> modificator;
-		brightness(modificator, image);
+		basicoperations(modificator, image, operations[0]);
 		image.save("output.bmp");
 	}
 
+	if ((string)argv[2] == "--contrast")
+	{
+		cout << "Give level of contrast " << endl;
+		cin >> modificator;
+		basicoperations(modificator, image, operations[1]);
+		image.save("output.bmp");
+	}
 
-
-
-	
+	if ((string)argv[2] == "--negative")
+	{
+		basicoperations(0, image, operations[2]);
+		image.save("output.bmp");
+	}
 
 
 	return 0;
