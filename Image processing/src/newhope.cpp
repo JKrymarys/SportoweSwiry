68a93610a34f19282819b17d0ab76ba65fa519f5
Argh4k <wyrzuc.maciej@gmail.com>
2016-10-21 10:15:39 +0200

    EMPIRE STRIKES BACK

--- Image processing/src/newhope.cpp
+++ Image processing/src/newhope.cpp
@@ -22,6 +22,11 @@ int main(int argc, char * argv[]) {
 	CImg <float> image;
 
 
+	if (argc < 2 || argc > 3)
+	{
+		cout << "Invalid number of arguments, if you need help use --help argument";
+		return 0;
+	}
 
 	//CHECK THE ARGUMENTS
 	if (argc == 2)
@@ -30,13 +35,19 @@ int main(int argc, char * argv[]) {
 		{
 			ifstream help_file;
 			help_file.open("help.txt");
-			string line;
-			while (!help_file.eof())
+			if (help_file.is_open())
 			{
-				getline(help_file, line);
-				cout << line << endl;
+
+				string line;
+				while (!help_file.eof())
+				{
+					getline(help_file, line);
+					cout << line << endl;
+				}
+				help_file.close();
 			}
-			help_file.close();
+			else
+				cout << "Couldnt find help file";
 			return 0;
 		}
 		else
@@ -47,43 +58,69 @@ int main(int argc, char * argv[]) {
 			CImg<float> image_with_noise;
 			CImg<float> image_without_noise;
 
-			cout << "\n Give picture with and without noise" << endl;
-			cin >> name_without_noise;
-			cin >> name_with_noise;
-
-			image_without_noise.load(name_without_noise.c_str());
-			image_with_noise.load(name_with_noise.c_str());
+	
 
 
 			if ((string)argv[1] == "--mse")
 			{
+				cout << "\n Give picture with and without noise" << endl;
+				cin >> name_without_noise;
+				cin >> name_with_noise;
+
+				image_without_noise.load(name_without_noise.c_str());
+				image_with_noise.load(name_with_noise.c_str());
 				Mean_square_error(image_without_noise, image_with_noise);
 
 				return 0;
 			}
 			else if ((string)argv[1] == "--pmse") {
+				cout << "\n Give picture with and without noise" << endl;
+				cin >> name_without_noise;
+				cin >> name_with_noise;
+
+				image_without_noise.load(name_without_noise.c_str());
+				image_with_noise.load(name_with_noise.c_str());
 				Peak_mean_square_error(image_without_noise, image_with_noise);
 				return 0;
 
 			}
 			else if ((string)argv[1] == "--snr")
 			{
+				cout << "\n Give picture with and without noise" << endl;
+				cin >> name_without_noise;
+				cin >> name_with_noise;
+
+				image_without_noise.load(name_without_noise.c_str());
+				image_with_noise.load(name_with_noise.c_str());
 				Signal_to_noise_error(image_without_noise, image_with_noise);
 					return 0;
 			}
 			else if((string)argv[1]=="--psnr")
 			{
+				cout << "\n Give picture with and without noise" << endl;
+				cin >> name_without_noise;
+				cin >> name_with_noise;
+
+				image_without_noise.load(name_without_noise.c_str());
+				image_with_noise.load(name_with_noise.c_str());
 				Peak_signal_to_noise_error(image_without_noise,image_with_noise);
 				return 0;
 			}
 			else if ((string)argv[1] == "--md")
 			{
+				cout << "\n Give picture with and without noise" << endl;
+				cin >> name_without_noise;
+				cin >> name_with_noise;
+
+				image_without_noise.load(name_without_noise.c_str());
+				image_with_noise.load(name_with_noise.c_str());
 				Maximum_difference(image_without_noise,image_with_noise);
 				return 0; 
 			}
 			else
 			{
 				cout << "Invalid operation\n If you need help, run program with --help parameter" << endl;
+				return 0;
 			}
 		}
 		
